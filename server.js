c2b6a08a30fd9ca5eaaead57daa2e56bb71bb76b
Leocadio Cruz Garcia <leo_algaba@hotmail.com>
2016-01-04 19:35:29 +0100

    Conectarnos a MongoDB
    Hacemos la conexion a la base de datos

--- server.js
+++ server.js
@@ -4,19 +4,25 @@ var express         = require("express"),
     methodOverride  = require("method-override"),
     mongoose        = require('mongoose');
 
+// Conectando a la  DB
+mongoose.connect('mongodb://localhost/seriestv', function(err, res) {
+  if(err) console.log('ERROR: Conectando a la BD:' + err);
+  else console.log('Conectando a la  BD');
+});
+
     // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-// Example Route
+// Ejemplo de ruteo
 var router = express.Router();
 router.get('/', function(req, res) {
   res.send("Hello world!");
 });
 app.use(router);
 
-// Start server
+// Empieza el servidor
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
 });
